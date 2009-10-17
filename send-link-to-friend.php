f15c4da752e3378a11ea678d0e79fd91ba1efec9
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-17 07:32:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/send-link-to-friend/tags/2.0@164181 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ send-link-to-friend.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+Plugin Name: Send link to friend
+Description: If the user thought the page link is useful to their friend, he can send the page link via this plug-in.. 
+Author: Gopi.R
+Version: 2.0
+Plugin URI: http://gopi.coolpage.biz/demo/2009/08/14/send-link-to-friend/
+Author URI: http://gopi.coolpage.biz/demo/2009/08/14/send-link-to-friend/
+Donate link: http://gopi.coolpage.biz/demo/2009/08/14/send-link-to-friend/
+*/
+
+function gSendtofriend()
+{
+	if(is_home() && get_option('gSendtofriend_On_Homepage') == 'YES') {	$display = "show";	}
+	if(is_single() && get_option('gSendtofriend_On_Posts') == 'YES') {	$display = "show";	}
+	if(is_page() && get_option('gSendtofriend_On_Pages') == 'YES') {	$display = "show";	}
+	if(is_archive() && get_option('gSendtofriend_On_Archives') == 'YES') {	$display = "show";	}
+	if(is_search() && get_option('gSendtofriend_On_Search') == 'YES') {	$display = "show";	}
+	
+	if($display == "show")
+	{
+		$sendlinks = "http://" . $_SERVER['HTTP_HOST']  . $_SERVER['REQUEST_URI'];
+		$_sm = get_option('gSendtofriend_title_sm');
+		?>
+        <link rel='stylesheet' href='<?php echo get_option('siteurl'); ?>/wp-content/plugins/send-link-to-friend/style.css' type='text/css' />
+		<form action="#" name="sendtofriendform" id="sendtofriendform">
+		<?php
+		if($_sm <> "")
+		{
+			echo "<div style='padding-top:4px;'>";
+			echo $_sm;    
+			echo "</div>";
+		}
+		?>
+		<div style='padding-top:4px;'><span id="send-link-to-friend-result"></span></div>
+		<div class="gtitle" style='padding-top:4px;'>Friend Email</div>
+		<div><input name="txt_friendemail" class="gtextbox" type="text" id="txt_friendemail" maxlength="120"></div>
+		<div class="gtitle" style='padding-top:4px;'>Enter your message</div>
+		<div><textarea name="txt_friendmessage" class="gtextarea" rows="3" id="txt_friendmessage"></textarea></div>
+		<div style="padding-top:4px;" ><input type="button" name="button" value="Send Link" onclick="javascript:get(this.parentNode,'<?php echo get_option('siteurl'); ?>/wp-content/plugins/send-link-to-friend/');"></div>
+		<input type="hidden" name="sendlink" id="sendlink" value="<?php echo $sendlinks; ?>"  />
+		</form>
+		<script language="JavaScript" src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/send-link-to-friend/send-link-to-friend.js"></script>
+		<?php
+	}
+}
+
+function gSendtofriend_install() 
+{
+
+	add_option('gSendtofriend_title', "Send link to Friend");
+	add_option('gSendtofriend_title_sm', "If you thought this page is useful to your friend, use this form to send.");
+	add_option('gSendtofriend_fromemail', "admin@sendtofriend.com");
+	add_option('gSendtofriend_On_Homepage', "YES");
+	add_option('gSendtofriend_On_Posts', "YES");
+	add_option('gSendtofriend_On_Pages', "YES");
+	add_option('gSendtofriend_On_Archives', "NO");
+	add_option('gSendtofriend_On_Search', "NO");
+}
+
+function gSendtofriend_widget($args) 
+{
+	if(is_home() && get_option('gSendtofriend_On_Homepage') == 'YES') {	$display = "show";	}
+	if(is_single() && get_option('gSendtofriend_On_Posts') == 'YES') {	$display = "show";	}
+	if(is_page() && get_option('gSendtofriend_On_Pages') == 'YES') {	$display = "show";	}
+	if(is_archive() && get_option('gSendtofriend_On_Archives') == 'YES') {	$display = "show";	}
+	if(is_search() && get_option('gSendtofriend_On_Search') == 'YES') {	$display = "show";	}
+	
+	if($display == "show")
+	{
+		extract($args);
+		echo $before_widget . $before_title;
+		echo get_option('gSendtofriend_title');
+		echo $after_title;
+		gSendtofriend();
+		echo $after_widget;
+	}
+}
+	
+function gSendtofriend_control() 
+{
+	echo '<p>Send link to friend.<br> To change the setting go to Send link to friend link under SETTING TAB.';
+	echo '<br><a href="options-general.php?page=send-link-to-friend/setting.php">';
+	echo 'click here</a>.</p>';
+}
+
+function gSendtofriend_widget_init()
+{
+  	register_sidebar_widget(__('Send link to friend'), 'gSendtofriend_widget');   
+	
+	if(function_exists('register_sidebar_widget')) 
+	{
+		register_sidebar_widget('Send link to friend', 'gSendtofriend_widget');
+	}
+	
+	if(function_exists('register_widget_control')) 
+	{
+		register_widget_control(array('Send link to friend', 'widgets'), 'gSendtofriend_control', 400, 400);
+	} 
+}
+
+function gSendtofriend_deactivation() 
+{
+	delete_option('gSendtofriend_title');
+	delete_option('gSendtofriend_title_sm');
+	delete_option('gSendtofriend_fromemail');
+	delete_option('gSendtofriend_On_Homepage');
+	delete_option('gSendtofriend_On_Posts');
+	delete_option('gSendtofriend_On_Pages');
+	delete_option('gSendtofriend_On_Archives');
+	delete_option('gSendtofriend_On_Search');
+}
+
+function gSendtofriend_add_to_menu() 
+{
+	add_options_page('Software', 'Send link to friend', 7, "send-link-to-friend/setting.php",'' );
+}
+
+add_action('admin_menu', 'gSendtofriend_add_to_menu');
+add_action("plugins_loaded", "gSendtofriend_widget_init");
+register_activation_hook(__FILE__, 'gSendtofriend_install');
+register_deactivation_hook(__FILE__, 'gSendtofriend_deactivation');
+add_action('init', 'gSendtofriend_widget_init');
+?>
